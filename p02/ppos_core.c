ef65486a583aba17df1f251f248285749ef98176
Eduardo Gabriel Kenzo Tanaka <eduduardogabriel888@gmail.com>
2024-03-27 22:54:46 -0300

    p02

--- p02/ppos_core.c
+++ p02/ppos_core.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int task_count = 0;  // contador para id das tarefas
-task_t *curr_task;   // ponteiro para tarefa corrente
-task_t main_task;    // tarefa main 
+int task_id_count = 0;  // contador para id das tarefas
+task_t *curr_task;      // ponteiro para tarefa corrente
+task_t main_task;       // tarefa main 
 
 // funções gerais ==============================================================
 
@@ -16,7 +16,7 @@ void ppos_init () {
 
     // inicializando a tarefa main
     getcontext(&(main_task.context));
-    main_task.id = task_count++;
+    main_task.id = task_id_count++;
     main_task.next = NULL;
     main_task.prev = NULL;
     main_task.status = 0;
@@ -66,7 +66,7 @@ int task_init (task_t *task, void (*start_func)(void *), void *arg) {
     // contexto da tarefa alterada para a função especificada
     makecontext(&(task->context), (void *) start_func, 1, arg);
 
-    task->id = task_count++;
+    task->id = task_id_count++;
     task->next = NULL;
     task->prev = NULL;
     task->status = 0;
@@ -118,87 +118,5 @@ int task_switch (task_t *task) {
     }
     return 0;
 }
-
-// suspende a tarefa atual,
-// transferindo-a da fila de prontas para a fila "queue"
-void task_suspend (task_t **queue) ;
-
-// acorda a tarefa indicada,
-// trasferindo-a da fila "queue" para a fila de prontas
-void task_awake (task_t *task, task_t **queue) ;
-
-// operações de escalonamento ==================================================
-
-// a tarefa atual libera o processador para outra tarefa
-void task_yield () ;
-
-// define a prioridade estática de uma tarefa (ou a tarefa atual)
-void task_setprio (task_t *task, int prio) ;
-
-// retorna a prioridade estática de uma tarefa (ou a tarefa atual)
-int task_getprio (task_t *task) ;
-
-// operações de gestão do tempo ================================================
-
-// retorna o relógio atual (em milisegundos)
-unsigned int systime () ;
-
-// suspende a tarefa corrente por t milissegundos
-void task_sleep (int t) ;
-
-// operações de sincronização ==================================================
-
-// a tarefa corrente aguarda o encerramento de outra task
-int task_wait (task_t *task) ;
-
-// inicializa um semáforo com valor inicial "value"
-int sem_init (semaphore_t *s, int value) ;
-
-// requisita o semáforo
-int sem_down (semaphore_t *s) ;
-
-// libera o semáforo
-int sem_up (semaphore_t *s) ;
-
-// "destroi" o semáforo, liberando as tarefas bloqueadas
-int sem_destroy (semaphore_t *s) ;
-
-// inicializa um mutex (sempre inicialmente livre)
-int mutex_init (mutex_t *m) ;
-
-// requisita o mutex
-int mutex_lock (mutex_t *m) ;
-
-// libera o mutex
-int mutex_unlock (mutex_t *m) ;
-
-// "destroi" o mutex, liberando as tarefas bloqueadas
-int mutex_destroy (mutex_t *m) ;
-
-// inicializa uma barreira para N tarefas
-int barrier_init (barrier_t *b, int N) ;
-
-// espera na barreira
-int barrier_wait (barrier_t *b) ;
-
-// destrói a barreira, liberando as tarefas
-int barrier_destroy (barrier_t *b) ;
-
-// operações de comunicação ====================================================
-
-// inicializa uma fila para até max mensagens de size bytes cada
-int mqueue_init (mqueue_t *queue, int max, int size) ;
-
-// envia uma mensagem para a fila
-int mqueue_send (mqueue_t *queue, void *msg) ;
-
-// recebe uma mensagem da fila
-int mqueue_recv (mqueue_t *queue, void *msg) ;
-
-// destroi a fila, liberando as tarefas bloqueadas
-int mqueue_destroy (mqueue_t *queue) ;
-
-// informa o número de mensagens atualmente na fila
-int mqueue_msgs (mqueue_t *queue) ;
 
 //==============================================================================
